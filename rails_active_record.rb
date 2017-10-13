8bbf75568dec5face12fca8349bb89d073b91670
Sumit Asok <sumitasok@gmail.com>
2017-10-13 23:31:38 +0530

    rails active record

--- rails_active_record.rb
+++ rails_active_record.rb
@@ -1,3 +1,8 @@
+# https://chodounsky.net/2015/09/14/create-record-if-it-does-not-exist-in-activerecord/
+
+User.find_or_create_by(first_name: "John", last_name: "Smith")
+User.where(first_name: "John", last_name: "Smith").first_or_create
+
 # bulk update
 # https://stackoverflow.com/questions/4912510/rails-3-activerecord-the-best-way-to-mass-update-a-single-field-for-all-the
 
