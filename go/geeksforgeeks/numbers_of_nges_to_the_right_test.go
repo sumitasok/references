87f7577746ec6f60f79b4369a7da0b058beab614
Sumit Asok <sumitasok@gmail.com>
2017-10-31 12:33:57 +0530

    golang NGE test using anonymous struct

--- go/geeksforgeeks/numbers_of_nges_to_the_right_test.go
+++ go/geeksforgeeks/numbers_of_nges_to_the_right_test.go
@@ -7,17 +7,15 @@ import (
 )
 
 func TestNge(t *testing.T) {
-	type inputOutput struct {
+	data := []struct {
 		input  []int
 		output []int
-	}
-
-	data := []inputOutput{
-		inputOutput{
+	}{
+		{
 			input:  []int{3, 4, 2},
 			output: []int{3, 4},
 		},
-		inputOutput{
+		{
 			input:  []int{3, 4, 2, 7, 5, 8, 10, 6},
 			output: []int{3, 4, 7, 8, 10},
 		},
