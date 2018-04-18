801085072717dad8a6236f405100f1bb74ef44af
Sumit Asok <sumitasok@gmail.com>
2018-04-18 14:00:18 +0530

    pattern/factory/ruby : in cases where a new company has to be added, and most of its functionalities are same as Default, we extend from Default.

--- patterns/factory/ruby/company/ibm/buy.rb
+++ patterns/factory/ruby/company/ibm/buy.rb
@@ -1,9 +1,12 @@
 module Company
   module Ibm
     module Buy
-      def products(company, params = {})
-        return "Company::Ibm::Buy.products #{company} #{params}"
-      end
+      extend Company::Default::Buy
+
+      # def products(company, params = {})
+      #   return "Company::Ibm::Buy.products #{company} #{params}"
+      # end
     end
   end
 end
+# Ref: https://stackoverflow.com/questions/5487414/ruby-extend-module-inside-another-module?utm_medium=organic&utm_source=google_rich_qa&utm_campaign=google_rich_qa
