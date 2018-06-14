cc3e209087c5ca220397193197e1a0fffc90ca39
Sumit Asok <sumitasok@gmail.com>
2018-06-14 12:37:55 +0530

    pandas

--- pandas/sample.py
+++ pandas/sample.py
@@ -5,10 +5,23 @@ from sqlalchemy import create_engine
 # import time
 # time.sleep(12)
 
-mysql_conn_string = 'mysql+pymysql://root@db:3306/sample'
+mysql_conn_string = 'mysql+pymysql://root:P09L123cedS@mydb:3306/iam'
 db = create_engine(mysql_conn_string)
 
 import pandas as pd
 
 df_verses = pd.read_sql('SELECT LEAST(3, 12, 34, 8, 25)', db)
 print(df_verses)
+
+
+import SimpleHTTPServer
+import SocketServer
+
+PORT = 2000
+
+Handler = SimpleHTTPServer.SimpleHTTPRequestHandler
+
+httpd = SocketServer.TCPServer(('', PORT), Handler)
+
+print('serving at port', PORT)
+httpd.serve_forever()
